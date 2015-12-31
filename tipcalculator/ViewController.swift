e1f609a583173335dd56e5f0a0c43d9797e8cdf8
Harris Christiansen (hc15ret2) <harrischristiansen@mac.com>
2015-12-31 12:18:41 -0700

    Initial Commit

---
+++ tipcalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipcalculator
+//
+//  Created by Harris Christiansen on 12/31/15.
+//  Copyright © 2015 Harris Christiansen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
